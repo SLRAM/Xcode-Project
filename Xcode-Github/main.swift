945e20dbba1db4eddd97cacd482079bd64e9c9a5
SLRAM <stephier2290@gmail.com>
2018-10-05 14:54:23 -0400

    initial commit of my xcode project

---
+++ Xcode-Github/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Xcode-Github
+//
+//  Created by Stephanie Ramirez on 10/5/18.
+//  Copyright © 2018 Stephanie Ramirez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
